017368c94951baa088e59ad63a8e8aac17c08079
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:49:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,8 @@ def long_planeteer_calls(array)
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  return array.find{|item| item in cheese_types}
 end
