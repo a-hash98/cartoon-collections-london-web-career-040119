7c96943634305fc376d773703e0b76d40de6d413
Ay Has <ahashim983@yahoo.com>
2019-03-29 00:40:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,19 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(d_names)
+  d_names.each_with_index{|name,index| puts "#{index+1}. #{name}"}
+  # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  r_array = Array.new
+  array.collect do |item|
+    r_array.push("#{item.capitalize}!")
+  end
+  return r_array
 end
+  # code an argument here
+  # Your code here
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
